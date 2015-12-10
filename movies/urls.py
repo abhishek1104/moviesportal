ae498bb963d0f72c04d74e3d0e80cd65c243e613
abhishek <abhishek1104@gmail.com>
2015-12-10 18:01:07 +0530

    updating views.py along with models

---
+++ movies/urls.py
@@ -0,0 +1,3 @@
+from django.conf.urls import url
+from . import views #Import views from same directory!
+
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse,HttpResponseRedirect
+from django.template import RequestContext,loader
+from .models import movies
+from django.core.urlresolvers import reverse
+from django.views import generic
+from django.utils import timezone
+from datetime import datetime
 
 # Create your views here.
+
+
+class IndexView(generic.ListView): #generic.ListView => Display list of objects!
+    template_name='movies/index.html'
+    context_object_name = 'thisweek_movies_released'
+
+    def get_queryset(self):
+        return movies.objects.filter(release_date__gte = timezone.now() - datetime.timedelta(days=7)).order_by('-release_date')[:5]
+
+
+class MovieDetailView(generic.DetailView):
+
+    model = movies
+    template_name = 'movies/details.html'
+
+    def get_queryset(self):
+        return movies.objects.filter
+
+
+def SelectMovieView(request,movies_id):
+    p= get_object_or_404(movies,pk=movies_id)
+
